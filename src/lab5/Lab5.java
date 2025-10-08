1d90b4914dbbcd7ae755bd6b0f87390922001aad
2389873 <2389873@host.docker.internal>
2025-10-08 10:53:15 -0400

    third commit completed task 1

--- src/lab5/Lab5.java
+++ src/lab5/Lab5.java
@@ -33,25 +33,28 @@ public class Lab5 extends Application{
         GridPane gp = new GridPane();
         Label label1 = new Label("Select Bag Style: ");
         Label label2 = new Label("Select Quantity: ");
-        ListView lv = new ListView();
+        ListView<String> lv = new ListView();
         lv.getItems().addAll("Full decorative", "Beaded", "Pirate Design", "Fringed", "Leather", "Plain");
         
-        ComboBox cb = new ComboBox();
-        cb.getItems().addAll(1,2,3,4,5,6,7,8,9,10);
+        ComboBox<String> cb = new ComboBox();
+        cb.getItems().addAll("1","2","3","4","5","6","7","8","9","10");
         gp.setAlignment(Pos.CENTER);
         gp.setHgap(30);
         
         gp.add(label1, 0, 0);
         gp.add(lv, 1, 0);
         GridPane gp2 = new GridPane();
-        gp2.add(label2, 0, 0);
-        gp2.add(cb, 1, 0);
+        HBox hb1 = new HBox();
+        hb1.getChildren().addAll(label2,cb);
+        gp2.add(hb1, 0, 0);
+       
         
         Button btn1 = new Button("Place Order");
         Button btn2 = new Button("Clear Selections");
-       
-        gp.add(btn1, 2, 2);
-        gp.add(btn2, 3, 2);
+        HBox hb3 = new HBox();
+        hb3.getChildren().addAll(btn1,btn2);
+        gp.add(hb3, 2, 2);
+        
         
         RadioButton r1 = new RadioButton("Small");
         RadioButton r2 = new RadioButton("Medium");
@@ -61,19 +64,30 @@ public class Lab5 extends Application{
         r1.setToggleGroup(tg);
         r2.setToggleGroup(tg);
         r3.setToggleGroup(tg);
-        
-        gp2.add(r1, 0, 1);
-        gp2.add(r2, 1, 1);
-        gp2.add(r3, 2, 1);
+        HBox hb2 = new HBox();
+        hb2.getChildren().addAll(r1, r2, r3);
+        gp2.add(hb2, 0, 1);
         gp2.setAlignment(Pos.CENTER);
         gp2.setVgap(50);
         gp.add(gp2, 2, 0);
         
-        btn1.setOnAction(e -> {
+        Label lbl3 = new Label();
         
+       
+        btn1.setOnAction(e -> {
+            if (cb.getValue() != null && lv.getSelectionModel().getSelectedItem() != null && ((RadioButton)tg.getSelectedToggle()).getText() != null)
+        lbl3.setText("You ordered : "+ cb.getValue() + " " + lv.getSelectionModel().getSelectedItem()+ 
+               " bags at size: "+ ((RadioButton)tg.getSelectedToggle()).getText());
         });
+        btn2.setOnAction(e -> {
+            lv.getSelectionModel().clearSelection();
+            cb.getSelectionModel().clearSelection();
+            tg.getSelectedToggle().setSelected(false);
+            lbl3.setText("");
+        });
+        gp.add(lbl3, 1, 1);
         Scene s = new Scene(gp, 1000, 500);
-        stage.setTitle("First application");
+        stage.setTitle("Bag Order Form");
         stage.setScene(s);
         stage.show();
     }
